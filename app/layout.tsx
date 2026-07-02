56326bd18846e40916a0240b739c32cf2a68a6e4
Nabin <mailnawbeen@gmail.com>
2026-07-02 11:35:55 +0545

    fix: add google fonts as html <link> instead of css @import

--- app/layout.tsx
+++ app/layout.tsx
@@ -147,6 +147,9 @@ export default function RootLayout({
         <meta name="theme-color" content="#f5f7fa" media="(prefers-color-scheme: light)" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
